194cd3c772e780d8d18cdac6cafadbd9581b81b3
John Roberts <me@jonojr.tech>
2025-01-03 18:45:54 +1100

    Fix precommit hooks

--- closest_river/rivers/api/serialziers.py
+++ closest_river/rivers/api/serialziers.py
@@ -1,5 +1,4 @@
 from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import SerializerMethodField
 
 from closest_river.rivers.models import River
 from closest_river.rivers.models import RiverSection
